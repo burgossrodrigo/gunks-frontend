9c67eb4ce57fe3cb5d0524128ba580efcb0c647a
burgossrodrigo <rodrigoburgos@live.com>
2021-09-04 19:00:49 -0300

    shit were done

---
+++ build/precache-manifest.32620b06b06a8cc50b216cf4f00a0881.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fd463324d412db0ffa2d5f32ab69da9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc8ab6b2eae5eb57f8de",
+    "url": "/static/css/main.b4dbd6bd.chunk.css"
+  },
+  {
+    "revision": "7e69824283895b277ac3",
+    "url": "/static/js/2.7d17829c.chunk.js"
+  },
+  {
+    "revision": "cc8ab6b2eae5eb57f8de",
+    "url": "/static/js/main.8adca374.chunk.js"
+  },
+  {
+    "revision": "8e689cd4d3827e59490f",
+    "url": "/static/js/runtime-main.e9c2ae90.js"
+  },
+  {
+    "revision": "7651a2b0aa73ed89eea1d427f226ed8c",
+    "url": "/static/media/1.7651a2b0.jpg"
+  },
+  {
+    "revision": "71068989f7e24fc3c81368a3c281a73b",
+    "url": "/static/media/2.71068989.jpg"
+  },
+  {
+    "revision": "14b4a78dd0a3edd2c6ee6ae1402a20c5",
+    "url": "/static/media/3.14b4a78d.jpg"
+  }
+]);
